58b9b42f118df043bd335426cd38a28c4099293a
bo.zeng <bo.zeng@laxino.com>
2016-05-31 14:24:19 +0800

    ui template

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module BookShop
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
